aad4a12687032899946cd223cdc2d18b8fff6881
Ivica Sestric <ivica.sestric@external.t-mobile.at>
2018-12-12 09:23:17 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            I was there
+            I was there, in feature
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Changed
           </a>
         </header>
       </div>
